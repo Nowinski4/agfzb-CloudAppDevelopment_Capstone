5a6f96e849c41a4a6048d51eac934975725ab8a3
Nowinski4 <nowinskipatrycja4@gmail.com>
2023-06-01 10:06:52 -0400

    Final changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,29 +9,12 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
     path(route='', view=views.get_dealerships, name='index'),
-    path(route='index/', view=views.get_dealerships, name='index'),
-
-    # path for about view
+    path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
+    path('dealer/<int:id>/review', views.add_review, name='add_review'),
     path(route='about/', view=views.about, name='about'),
-
-
-    # path for contact us view
-    path(route='contact/',view=views.contact,name='contact'),
-
-    # path for registration
+    path(route='contact/', view=views.contact, name='contact'),
     path('registration/', views.registration_request, name='registration'),
-
-    # path for login
-    path(route='login/',view=views.login_request,name='login_request'),
-
-    # path for logout
-    path('logout/', views.logout_request, name='logout_request'),
-
-    # path for dealer reviews view
-    path(route='dealer/<int:id>/', view=views.get_dealer_details, name='dealer_details'),
-
-
-    # path for add a review view
-    path('dealer/<int:id>/add-review/', views.add_review, name='add_review')
-
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
